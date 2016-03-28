934102e1bc26a20e58f226ce05b55978cfbd82b0
timini <tim@rewire.it>
2016-03-28 12:47:33 +0100

    add url field to comments api

--- server/api/chat/serializers.py
+++ server/api/chat/serializers.py
@@ -4,12 +4,13 @@ from chat.models import Comment
 from users.models import User
 
 
-class StringToIntPK(object):
-    def to_representation(key):
-        return int(key)
-
 class CommentSerializer(serializers.ModelSerializer):
-    user = relations.ResourceRelatedField(queryset=User.objects.all(), pk_field=serializers.UUIDField(format='hex_verbose'), many=False, read_only=False)
+    user = relations.ResourceRelatedField(
+        queryset=User.objects.all(),
+        pk_field=serializers.UUIDField(format='hex_verbose'),
+        many=False,
+        read_only=False
+    )
     class Meta:
         model = Comment
-        fields = ('text', 'user', 'created_at', 'updated_at')
+        fields = ('url', 'text', 'user', 'created_at', 'updated_at')
